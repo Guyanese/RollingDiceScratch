f9133c66e68adb75faac976d13d514f9b46145a6
Guyanese <xRyanAlly@gmail.com>
2014-11-24 13:06:55 -0500

    Update

--- app/src/main/java/com/rana/rollingdicescratch/StartGame.java
+++ app/src/main/java/com/rana/rollingdicescratch/StartGame.java
@@ -1,59 +1,33 @@
 package com.rana.rollingdicescratch;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.app.Activity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 /**
  * Created by Ryan on 11/5/2014.
  */
 
 public class StartGame extends Activity {
-    private int nNumOfDice;
-    private int nNumOfSides;
+    Button btnRoll;
+    ImageView imgdice1;
 
-    int[] arDice1 = new int[]{R.drawable.dice1, R.drawable.dice2,
-            R.drawable.dice3, R.drawable.dice4, R.drawable.dice5, R.drawable.dice6};
-
-    int[] arDice2 = new int[]{R.drawable.secdice1, R.drawable.secdice2,
-            R.drawable.secdice3, R.drawable.secdice4, R.drawable.secdice5, R.drawable.secdice6};
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.startgame);
 
-        nNumOfDice=0;
-        nNumOfSides=0;
-    }
-    public void RollDice(View v) {
-        //http://stackoverflow.com/questions/20382521/using-multiple-button-to-start-different-activities-android
-        //Used this source to help  simplify the button code.
-        switch (v.getId()) {
-            case R.id.btnRollDice:
-                rollDice1();
-                break;
-        }
-    }
-    //https://github.com/Kapin/Dice-Roller/blob/master/src/com/kapin/diceroller/DiceRollerActivity.java#L220
-    private void rollDice1()
-    {
-        int diceTotal=0;
-        String diceRolls="";
-        for(int i = 0;i<arDice1.length;++i){
-            int temp = diceRoll();
-            diceTotal+=temp;
-            diceRolls+=(temp+"");
-            if(i<nNumOfDice-1)
-            {
-                diceRolls+=" ";
+        btnRoll = (Button) findViewById(R.id.btnRollDice);
+        imgdice1 = (ImageView) findViewById(R.id.imgdice1);
+
+        btnRoll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imgdice1.setImageResource(R.drawable.dice2);
             }
-        }
-    }
-    private int diceRoll(){
-        return ((int)(Math.random()*nNumOfSides+1));
+        });
     }
 }
-
-
 
