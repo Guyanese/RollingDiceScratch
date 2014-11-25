d9c2264acd45e8afa22261ae96594b0985e374c9
Guyanese <xRyanAlly@gmail.com>
2014-11-24 22:54:46 -0500

    Update

--- app/src/main/java/com/rana/rollingdicescratch/StartGame.java
+++ app/src/main/java/com/rana/rollingdicescratch/StartGame.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.util.Random;
+
 /**
  * Created by Ryan on 11/5/2014.
  */
@@ -13,6 +15,7 @@ import android.widget.ImageView;
 public class StartGame extends Activity {
     Button btnRoll;
     ImageView imgdice1;
+    Random random = new Random();
     int count = -1;
     private int[] arDiceImages = { R.drawable.imgdice1,R.drawable.imgdice2, R.drawable.imgdice3,
             R.drawable.imgdice4, R.drawable.imgdice5,  R.drawable.imgdice6 };
@@ -24,13 +27,17 @@ public class StartGame extends Activity {
         setContentView(R.layout.startgame);
 
         btnRoll = (Button) findViewById(R.id.btnRollDice);
+       // https://www.youtube.com/watch?v=O6Tad0BlqUY&index=3&list=PLvnXjBkwUhDEfjK1pqT8LsGWPyFuzO5Zu
         imgdice1 = (ImageView) findViewById(R.id.imgdice1);
 
         btnRoll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //https://www.youtube.com/watch?v=yLwhzIPPYxo - Just used the code he used, here.
                 count = count + 1;
                 imgdice1.setImageResource(arDiceImages[count]);
+                //int index = random.nextInt(arDiceImages[]);
+                //imgdice1.setImageResource(index);
             }
         });
     }
